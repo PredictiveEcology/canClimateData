f3be56ff6f1182c04f66a28411aa4830fac6e569
Alex Chubaty <achubaty@for-cast.ca>
2023-12-06 23:54:18 -0700

    [WIP] script ClimateNA processing using tiles

--- canClimateData.R
+++ canClimateData.R
@@ -18,7 +18,7 @@ defineModule(sim, list(
   documentation = deparse(list("README.md", "canClimateData.Rmd")),
   reqdPkgs = list("archive", "digest", "geodata", "googledrive", "purrr",
                   "R.utils", "sf", "spatialEco", "terra",
-                  "PredictiveEcology/climateData@development (>= 1.0.1)",
+                  "PredictiveEcology/climateData@development (>= 1.0.6)",
                   "PredictiveEcology/fireSenseUtils@development (>= 0.0.5.9046)",
                   "PredictiveEcology/LandR@development (>= 1.1.0.9064)",
                   "PredictiveEcology/reproducible@development (>= 2.0.8.9001)",
