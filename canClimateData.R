5c3681bcdbb28a1165fa9d05176b3dc6e749f53b
Eliot McIntire <eliotmcintire@gmail.com>
2025-09-15 14:49:05 -0700

    bump minimum version climateData

--- canClimateData.R
+++ canClimateData.R
@@ -20,7 +20,7 @@ defineModule(sim, list(
   documentation = deparse(list("README.md", "canClimateData.Rmd")),
   reqdPkgs = list("archive", "digest", "geodata", "googledrive", "purrr",
                   "R.utils", "sf", "spatialEco", "terra",
-                  "PredictiveEcology/climateData@modsDuringFireSense (>= 2.2.2.9000)",
+                  "PredictiveEcology/climateData@modsDuringFireSense (>= 2.2.2.9002)",
                   "PredictiveEcology/fireSenseUtils@development (>= 0.0.5.9046)",
                   "PredictiveEcology/LandR@development (>= 1.1.0.9064)",
                   "PredictiveEcology/reproducible@development (>= 2.1.1.9002)", ##
